ac4d45711274ca75a7bfb32cd31f852fb26364c4
luanalimadeveloper <luanalima.developer@gmail.com>
2022-06-18 11:17:59 -0300

    GET All genres

---
+++ src/main/java/com/devsuperior/movieflix/services/GenreService.java
@@ -0,0 +1,24 @@
+package com.devsuperior.movieflix.services;
+
+import com.devsuperior.movieflix.dto.GenreDTO;
+import com.devsuperior.movieflix.entities.Genre;
+import com.devsuperior.movieflix.repositories.GenreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class GenreService {
+
+    @Autowired
+    private GenreRepository repository;
+
+    public List<GenreDTO> findAll() {
+        List<Genre> list = repository.findAll(Sort.by("name"));
+        return list.stream().map(x-> new GenreDTO(x)).collect(Collectors.toList());
+    }
+
+}
